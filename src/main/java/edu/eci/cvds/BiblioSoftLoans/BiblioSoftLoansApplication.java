662ebd9f23171f1f589f6e8cec9ab813ffb6f02d
Samuel Felipe Diaz <samuelfdm14@gmail.com>
2024-11-07 14:35:23 -0500

    Estructura inicial del modulo Loans

---
+++ src/main/java/edu/eci/cvds/BiblioSoftLoans/BiblioSoftLoansApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.BiblioSoftLoans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioSoftLoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioSoftLoansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/BiblioSoftLoans/BiblioSoftLoansApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.BiblioSoftLoans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioSoftLoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
